afed6829fa825c388ffaffa844752558f4a4f988
SammyLight <59570031+SammyLight@users.noreply.github.com>
2026-06-15 01:47:00 +0100

    Update liteYtEmbed.js

--- script/victorScripts/liteYtEmbed.js
+++ script/victorScripts/liteYtEmbed.js
@@ -219,11 +219,11 @@ function runFirstPartOfCode() {
                         }
 
                         // Fetch additional video details from YouTube Data API v3
-                        const apiKey = 'AIzaSyBQjKp7Xt5hFJHYwPCjM1wBXg6KBBZrayo';
+                        const apiKey = 'AIzaSyDpDUtcguArU8tt5d8J5w65Nw-pkno-UoI';
                         const videoDetailsURL = `https://www.googleapis.com/youtube/v3/videos?id=${this.videoId}&part=snippet,contentDetails,liveStreamingDetails&key=${apiKey}`;            
                         fetch(videoDetailsURL, {
                             headers: {
-                                'Referer': 'https://*.lightcityteachingfellowship.github.io/BibleStudies/' // Replace with your GitHub Pages URL
+                                'Referer': 'https://*.lightcityfellowship.com' // Replace with your website URL
                             }
                         })
                         .then(response => response.json())
